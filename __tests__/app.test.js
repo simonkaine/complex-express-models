4dd888113917972dc06209fe267bca30a95eb3f9
Simon Kaine <simonkaine@gmail.com>
2021-10-04 16:21:22 -0700

    Route to add a new Species + pass test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,21 @@ describe('demo routes', () => {
     return setup(pool);
   });
 
+  it('Should add/save a new Species', () => {
+    const type = {
+      type: 'mammal',
+      typeId: null
+    };
+
+    return request(app).post('/api/species').send(type)
+      .then((res) => { expect(res.body).toEqual({
+        ...type,
+        id: '1'
+      });
+      });
+  });
+
+
   afterAll(() => {
     pool.end();
   });
