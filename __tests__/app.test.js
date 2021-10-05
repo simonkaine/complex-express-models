28a55003001009c0eab9ec14e7aa0ff068e31621
Simon Kaine <simonkaine@gmail.com>
2021-10-04 19:01:38 -0700

    Route to add a new Animal + pass test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -34,18 +34,27 @@ describe('demo routes', () => {
       });
   });
 
-  // it('Should add/save a new animal', () => {
-  //   const animal1 = {
-  //     name: 'deer',
-  //     nickname: 'sea deer'
-  //   };
-  //   return request(app).post('/api/animal').send(animal1)
-  //     .then((res) => { expect(res.body).toEqual({
-  //       ...animal1,
-  //       id: '1'
-  //     });
-  //     });
-  // });
+  it('Should add/save a new animal', async () => {
+    const species1 = {
+      type: 'mammal'
+    };
+
+    const animal1 = {
+      name: 'deer',
+      nickname: 'sea deer',
+      typeId: '1'
+    };
+    await request(app).post('/api/species').send(species1);
+    return await request(app).post('/api/animals').send(animal1)
+    
+      .then((res) => { 
+        console.log(res.body);
+        expect(res.body).toEqual({
+          ...animal1,
+          id: '1'
+        });
+      });
+  });
 
   afterAll(() => {
     pool.end();
